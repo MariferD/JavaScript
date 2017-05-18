43f49060780c22f337af4a5478d3d1b1433d93bb
MariferD <fer_chicamoxa@hotmail.com>
2017-05-18 01:01:29 -0500

    Calculadora terminada

--- js/java.js
+++ js/java.js
@@ -1,6 +1,35 @@
-var n=0;
-function texto()
+var n1=0,n2=0,resultado=0
+function suma()
 {
-	document.getElementById('main').innerHTML='mi primer funcion' +n 
-	n++
+	var n1=document.getElementById("N1").value;
+	var n2=document.getElementById("N2").value;
+	var resultado=parseInt(n1,10)+parseInt(n2,10)
+	document.getElementById("resultado").value=resultado;
 }
+var n1=0,n2=0,resultado=0
+function resta()
+{
+	var n1=document.getElementById("N1").value;
+	var n2=document.getElementById("N2").value;
+	var resultado=parseInt(n1,10)-parseInt(n2,10)
+	document.getElementById("resultado").value=resultado;
+
+}
+var n1=0,n2=0,resultado=0
+function multiplicar()
+{
+	var n1=document.getElementById("N1").value;
+	var n2=document.getElementById("N2").value;
+	var resultado=parseInt(n1,10)*parseInt(n2,10)
+	document.getElementById("resultado").value=resultado;
+
+}
+var n1=0,n2=0,resultado=0
+function dividir()
+{
+	var n1=document.getElementById("N1").value;
+	var n2=document.getElementById("N2").value;
+	var resultado=parseInt(n1,10)/parseInt(n2,10)
+	document.getElementById("resultado").value=resultado;
+}
+ 
